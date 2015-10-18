dd7fb1b774ebdb6cc5783f58758138673e9c54e4
Ivan <xarrper@gmail.com>
2015-10-18 23:15:15 +0400

    indexAutoload

--- index.php
+++ index.php
@@ -1,50 +1,31 @@
 <?
-require("section.php");
 
-$section = isset( $_GET['section'] ) ? $_GET['section'] : 1; //если секция не указано, то по умолчанию главная, т.е. 1.
-$section = (Sections::isSection($section)) ? $section : 0;
-admin($section);
-
-function info($section) { 
-	$data = Sections::getIdSection($section);
-	return $data;
-}
- 
-function tree($data,$parent_id) { //объеденить с деревом admin.php
-	if(is_array($data) and isset($data[$parent_id])){
-        $tree = '<ul>';
-		foreach($data[$parent_id] as $d){
-			$tree .= '<li><a href="/?section='.$d['id'].'">'.$d['name'].' #'.$d['id'];
-			$tree .=  tree($data,$d['id']);
-			$tree .= '</a></li>';
+	error_reporting (E_ALL); 
+	include('config.php');
+	
+	function search_file($folderName, $fileName, &$str){ //куданубдь в другое место
+		$dir = opendir($folderName);
+		while (($file = readdir($dir)) !== false){ 
+			if($file != "." && $file != ".." && $file != ".git"){ 
+				if(is_file($folderName."/".$file)){
+					if($file == $fileName) {
+						$str=$folderName."/".$file;
+					}
+				} 
+				if(is_dir($folderName."/".$file)) search_file($folderName."/".$file, $fileName, $str);
+			} 
 		}
-        $tree .= '</ul>';
-    }
-    else return null;
-	return $tree;
-}	
- 
-function menu($section) { 
-  $data = Sections::getIdSectionMenu($section);
-  return tree($data,0);
-}
+		closedir($dir);
+	}
+	
+	function __autoload($className) { //где прописать исключение если файла нет???
+		search_file(".", mb_strtolower($className.'.php'), $path);
+		if(file_exists($path)) require_once($path);
+		else return false;
+	}
 
-function breadCrumbs($section) {
-  $data = Sections::bread($section);
-  $str="";
-  for ($i=count($data)-1; $i>=0; $i--) {
-	$str .="<a href='/?section=".$data[$i]['id']."'>".$data[$i]['name']."</a>/";
-  }
-  return $str;
-}
+	$a = new Router();
+	$a->run();
+	
 
-function admin($section) { //загрузка представления
-  $data = array();
-  $data = info($section);
-  $data['menu'] = menu($section);
-  $data['bread'] = breadCrumbs($section);
-  $data['page_name'] = 'Сайт';
-  require("view/viewSite.php");
-}
- 
 ?>
