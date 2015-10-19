d0729d583257586f9c8d9c27b29ae0060c1e8a48
Ivan <xarrper@gmail.com>
2015-10-19 08:20:23 +0400

    EnterPart

--- App/Controllers/Admin/EnterController.php
+++ App/Controllers/Admin/EnterController.php
@@ -1,7 +1,20 @@
 <?
 class EnterController {
+	
+	function __construct() {
+		$this->userView = new AllView(); //сделать в классе родителя!
+	}
+	
 	function action() {
-		echo 'вход';
+		@session_start();
+		if (isset($_SESSION['login']) and isset($_SESSION['password'])) {
+			echo 'админ!';
+			//header("Location: ".PATH."/admin.php"); 
+		}
+		else {
+			$data['page_name']='Вход';
+			$this->userView->show($data, 'EnterView.php');
+		}
 	}
 }
 ?>
---
+++ view/EnterView.php
@@ -0,0 +1,16 @@
+<?php include "view/header.php" ?>
+
+	<form name="test" method="post" action="/admin">
+		<div class='paddingContent'>
+			<h2 class="sizeMedium"><b>Логин</b><br>
+			<input type="text" name="login" size="40">
+			</h2>
+			<h2 class="sizeMedium"><b>Пароль</b><br>
+			<input type="password" name="password" size="40">
+			</h2>
+			<p><input type="submit" name="enter" value="Вход">
+		</div>
+	</form>
+</body>
+</html>
+
